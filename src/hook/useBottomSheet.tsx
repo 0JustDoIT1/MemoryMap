095c32181b8ad08fb2e86376db4dd2511ae6cf8a
0JustDoIT1 <0justdoit1@gmail.com>
2024-10-29 18:13:21 +0900

    Split BottomSheet (Custom / Map)

--- src/hook/useBottomSheet.tsx
+++ src/hook/useBottomSheet.tsx
@@ -1,7 +1,12 @@
 import {BottomSheetBackdrop, BottomSheetModal} from '@gorhom/bottom-sheet';
 import {useCallback, useMemo, useRef, useState} from 'react';
-import {KoreaRegionList} from 'src/constants/regionList';
-import {SettingBottomSheet, SettingMapSheet} from 'src/types/bottomSheet';
+
+interface SettingBottomSheet {
+  title: string;
+  description?: string;
+  contents: React.JSX.Element;
+  snap: string;
+}
 
 const useCustomBottomSheet = () => {
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
@@ -16,6 +21,7 @@ const useCustomBottomSheet = () => {
 
   // Bottom Sheet height setting [index0, index1]
   const snapPoints = useMemo(() => ['40%', point], [point]);
+
   // Bottom Sheet present event
   const handlePresentModalPress = useCallback(
     ({title, description, contents, snap}: SettingBottomSheet) => {
@@ -27,47 +33,14 @@ const useCustomBottomSheet = () => {
     },
     [bottomSheetTitle, bottomSheetDescription, bottomSheetContents, point],
   );
+
   // Bottom Sheet close event
   const handleClosePress = () => bottomSheetModalRef.current?.close();
+
   // Bottom Sheet close event when background touch
   const renderBackdrop = useCallback(
     (props: any) => <BottomSheetBackdrop {...props} pressBehavior="close" />,
     [],
-  );
-
-  /** --------------------Map Bottom Sheet--------------------------- */
-  const [tag, setTag] = useState<string[]>([]);
-
-  const handleMapModalPress = useCallback(
-    ({id, snap}: SettingMapSheet) => {
-      let result: any;
-
-      // 객체 속 원하는 값 찾기 (Defth First Search 방식, 중첩 객체도 가능)
-      const DFS = (obj: any, name: any, val: any) => {
-        if (obj[name] === val) result = obj;
-        else
-          Object.values(obj).forEach(value => {
-            if (typeof value === 'object') DFS(value, name, val);
-          });
-      };
-
-      DFS(KoreaRegionList, 'id', id);
-
-      const values = result.value;
-      const length = values.length;
-      const title = values[length - 1];
-
-      let tagList = [];
-      for (let i = 0; i < length - 1; i++) {
-        tagList.push(values[i]);
-      }
-
-      setBottomSheetTitle(title);
-      setTag(tagList);
-      setPoint(snap);
-      bottomSheetModalRef.current?.present();
-    },
-    [bottomSheetTitle, tag, point],
   );
 
   return {
@@ -77,10 +50,8 @@ const useCustomBottomSheet = () => {
     bottomSheetDescription,
     bottomSheetContents,
     handlePresentModalPress,
-    handleMapModalPress,
     handleClosePress,
     renderBackdrop,
-    tag,
   };
 };
 
---
+++ src/hook/useMapSheet.tsx
@@ -0,0 +1,66 @@
+import {BottomSheetBackdrop, BottomSheetModal} from '@gorhom/bottom-sheet';
+import {useCallback, useMemo, useRef, useState} from 'react';
+import {KoreaRegionList} from 'src/constants/regionList';
+import {SettingMapSheet} from 'src/types/mapSheet';
+
+const useMapSheet = () => {
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const [bottomSheetTitle, setBottomSheetTitle] = useState<string>('');
+  const [tag, setTag] = useState<string[]>([]);
+
+  // Bottom Sheet height setting [index0, index1]
+  const snapPoints = useMemo(() => ['30%', '40%'], []);
+
+  const handleMapModalPress = useCallback(
+    ({id}: SettingMapSheet) => {
+      let result: any;
+
+      // 객체 속 원하는 값 찾기 (Defth First Search 방식, 중첩 객체도 가능)
+      const DFS = (obj: any, name: any, val: any) => {
+        if (obj[name] === val) result = obj;
+        else
+          Object.values(obj).forEach(value => {
+            if (typeof value === 'object') DFS(value, name, val);
+          });
+      };
+
+      DFS(KoreaRegionList, 'id', id);
+
+      const values = result.value;
+      const length = values.length;
+      const title = values[length - 1];
+
+      let tagList = [];
+      for (let i = 0; i < length - 1; i++) {
+        tagList.push(values[i]);
+      }
+
+      setBottomSheetTitle(title);
+      setTag(tagList);
+      bottomSheetModalRef.current?.present();
+    },
+    [bottomSheetTitle, tag],
+  );
+
+  // Bottom Sheet close event
+  const handleClosePress = () => bottomSheetModalRef.current?.close();
+
+  // Bottom Sheet close event when background touch
+  const renderBackdrop = useCallback(
+    (props: any) => <BottomSheetBackdrop {...props} pressBehavior="close" />,
+    [],
+  );
+
+  return {
+    bottomSheetModalRef,
+    snapPoints,
+    bottomSheetTitle,
+    tag,
+    handleMapModalPress,
+    handleClosePress,
+    renderBackdrop,
+  };
+};
+
+export default useMapSheet;
