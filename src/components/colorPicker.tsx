945fbeb072386250fb742d98e106cd4fca88c12e
0JustDoIT1 <0justdoit1@gmail.com>
2024-10-29 18:20:34 +0900

    Re-Setting Color Picker Base

---
+++ src/components/colorPicker.tsx
@@ -0,0 +1,23 @@
+import {View} from 'react-native';
+import ColorPicker, {
+  HueSlider,
+  Panel1,
+  Preview,
+  returnedResults,
+} from 'reanimated-color-picker';
+interface CustomColorPicker {
+  value: string;
+  onComplete: ((colors: returnedResults) => void) | undefined;
+}
+const CustomColorPicker = ({value, onComplete}: CustomColorPicker) => {
+  return (
+    <ColorPicker value={value} onComplete={onComplete}>
+      <View className="flex-row justify-between">
+        <Panel1 style={{width: '85%'}} thumbSize={15} />
+        <HueSlider vertical thumbShape="pill" thumbColor="#000000" />
+      </View>
+      <Preview style={{marginTop: 10}} />
+    </ColorPicker>
+  );
+};
+export default CustomColorPicker;
---
+++ src/hook/useModal.tsx
@@ -0,0 +1,8 @@
+import {useState} from 'react';
+const useModal = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  return {visible, showModal, hideModal};
+};
+export default useModal;
