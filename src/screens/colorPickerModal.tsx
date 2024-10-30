02854af1e720d9c0f67240367f5262bc8f53770e
hbc3869 <hbc3869@gmail.com>
2024-10-30 22:41:14 +0900

    Edit Color Picker Step 1/2

--- src/screens/colorPickerModal.tsx
+++ src/screens/colorPickerModal.tsx
@@ -1,40 +1,46 @@
 import {useState} from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import {Text} from 'react-native-paper';
+import Animated, {useSharedValue} from 'react-native-reanimated';
 import {returnedResults} from 'reanimated-color-picker';
 import CustomColorPannel from 'src/components/colorPannel';
 import CustomColorSwatch from 'src/components/colorSwatch';
 import {KoreaRegionData} from 'src/types/koreaMap';
 
 const ColorPickerModal = ({background}: KoreaRegionData) => {
-  const [more, setMore] = useState<boolean>(false);
-  const [hex, setHex] = useState<string>(background);
+  const [mode, setMode] = useState<boolean>(false);
 
-  const onPickerColor = (color: returnedResults) => {
-    setHex(color.hex);
+  const selectedColor = useSharedValue(background);
+
+  const onChangeMode = () => {
+    setMode(!mode);
   };
-  const onSelectColor = () => {
-    console.log('###', hex);
+
+  const onColorSelect = (color: returnedResults) => {
+    'worklet';
+    selectedColor.value = color.hex;
   };
+  const onSelectColor = () => {};
+
   return (
-    <View>
+    <Animated.View>
       <Text className="text-lg mb-4">색상 선택</Text>
-      {more ? (
-        <CustomColorPannel value={background} onComplete={onPickerColor} />
+      {mode ? (
+        <CustomColorPannel value={background} onChange={onColorSelect} />
       ) : (
-        <CustomColorSwatch value={background} onComplete={onPickerColor} />
+        <CustomColorSwatch value={background} onChange={onColorSelect} />
       )}
       <View className="mt-4 flex-row justify-between items-center">
-        <TouchableOpacity activeOpacity={0.8} onPress={() => setMore(!more)}>
+        <TouchableOpacity activeOpacity={0.8} onPress={onChangeMode}>
           <Text className="text-brandMain">
-            {more ? '기본 색상' : '더 많은 색상'}
+            {mode ? '기본 색상' : '더 많은 색상'}
           </Text>
         </TouchableOpacity>
         <TouchableOpacity activeOpacity={0.8} onPress={onSelectColor}>
           <Text className="text-brandMain">선택</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </Animated.View>
   );
 };
 export default ColorPickerModal;
