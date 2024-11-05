f8e205c86e183db7577533951b7cae0ebd81baf7
hbc3869 <hbc3869@gmail.com>
2024-11-06 00:11:29 +0900

    Add Svg Pattern Component

--- src/components/svgPattern.tsx
+++ src/components/svgPattern.tsx
@@ -1,28 +1,40 @@
+import {useEffect} from 'react';
 import {Defs, Image, Pattern} from 'react-native-svg';
+import {useRecoilValue} from 'recoil';
 import useKoreaMap from 'src/hook/useKoreaMap';
+import {koreaMapDataState} from 'src/recoil/atom';
 
 const SvgPattern = () => {
-  const {getSvgDataById, getMapDataById} = useKoreaMap();
+  const koreaMapData = useRecoilValue(koreaMapDataState);
+  const {idArray, getSvgDataById, getMapDataById, getTypePhotoToIdArray} =
+    useKoreaMap();
+
+  useEffect(() => {
+    getTypePhotoToIdArray('photo');
+  }, [koreaMapData]);
 
   return (
     <Defs>
-      <Pattern
-        id="KR-13-18"
-        patternUnits="userSpaceOnUse"
-        x={getSvgDataById('KR-13-18').svgStyle?.x}
-        y={getSvgDataById('KR-13-18').svgStyle?.y}>
-        <Image
-          width={getSvgDataById('KR-13-18').svgStyle?.width}
-          height={getSvgDataById('KR-13-18').svgStyle?.height}
-          href={
-            'file:///data/user/0/com.memorymap/cache/rn_image_picker_lib_temp_38eff628-74ca-46b3-8d21-68df127b3430.jpg'
-          }
-          translateX={getMapDataById('KR-13-18').imageStyle?.x}
-          translateY={getMapDataById('KR-13-18').imageStyle?.y}
-          scale={getMapDataById('KR-13-18').imageStyle?.scale}
-          rotation={getMapDataById('KR-13-18').imageStyle?.rotation}
-        />
-      </Pattern>
+      {idArray.map(item => (
+        <Pattern
+          key={item}
+          id={item}
+          patternUnits="userSpaceOnUse"
+          x={getSvgDataById(item).svgStyle?.x}
+          y={getSvgDataById(item).svgStyle?.y}>
+          <Image
+            width={getSvgDataById(item).svgStyle?.width}
+            height={getSvgDataById(item).svgStyle?.height}
+            href={
+              'file:///data/user/0/com.memorymap/cache/rn_image_picker_lib_temp_38eff628-74ca-46b3-8d21-68df127b3430.jpg'
+            }
+            translateX={getMapDataById(item).imageStyle?.x}
+            translateY={getMapDataById(item).imageStyle?.y}
+            scale={getMapDataById(item).imageStyle?.scale}
+            rotation={getMapDataById(item).imageStyle?.rotation}
+          />
+        </Pattern>
+      ))}
     </Defs>
   );
 };
