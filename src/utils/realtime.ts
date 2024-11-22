bfb6ad091fbe28ac7ef5f419c3e6f341a7c3d0b2
hbc3869 <hbc3869@gmail.com>
2024-11-23 08:26:56 +0900

    Test Console

--- src/utils/realtime.ts
+++ src/utils/realtime.ts
@@ -5,17 +5,21 @@ import {AppData} from 'src/types/account';
 const reference = firebase.app().database(FireBaseDataBase);
 
 export const _setRealtime = (data: AppData) => {
+  console.log('리얼타임 생성');
   return reference.ref(`/users/${data.uid}`).set(data);
 };
 
 export const _updateRealtime = (data: AppData) => {
+  console.log('리얼타임 업데이트');
   return reference.ref(`/users/${data.uid}`).update(data);
 };
 
 export const _readRealtime = (uid: string) => {
+  console.log('리얼타임 읽기');
   return reference.ref(`/users/${uid}`).once('value');
 };
 
 export const _deleteRealtime = (uid: string) => {
+  console.log('리얼타임 삭제');
   return reference.ref(`/users/${uid}`).remove();
 };
