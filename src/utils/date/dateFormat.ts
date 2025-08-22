e22bae27e20a4b41e6a970bc3361045a80e98872
hbc3869 <0justdoit1@gmail.com>
2025-08-22 16:52:45 +0900

    Refactor utils/date

--- src/utils/date/dateFormat.ts
+++ src/utils/date/dateFormat.ts
@@ -1,5 +1,21 @@
 import {DateType} from 'react-native-ui-datepicker';
+import 'dayjs/locale/ko';
 import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.locale('ko');
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(isBetween);
+
+dayjs.tz.setDefault('Asia/Seoul');
+
+export type TDayjsType = dayjs.Dayjs;
+export type TDayjsConfigType = dayjs.ConfigType;
+
 // import {Timestamp} from '@react-native-firebase/firestore';
 
 // // Date를 Firebase Timestamp로 변환
@@ -22,10 +38,10 @@ import dayjs from 'dayjs';
 // DateType을 원하는 형식 String으로 변환
 export const dateToFormatString = (date: DateType, format: string) => {
   if (!date) return '';
-  return dayjs(date).format(format);
+  return dayjs(date).tz().format(format);
 };
 
 // calendar에서 DateType을 Date로 변환
 export const dateTypeToDate = (date: DateType) => {
-  return dayjs(date).toDate();
+  return dayjs(date).tz().toDate();
 };
